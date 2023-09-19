63369a5e9c29ade68cbe1278f012000bfecfd265
alexindev <eruslanov-opt@yandex.ru>
2023-09-19 11:58:03 +0300

    create logger configurations

---
+++ logs/config.py
@@ -0,0 +1,7 @@
+from loguru import logger
+from datetime import date
+
+logger.add(f'logs/{date.today()}.log', format='<white>{time:HH:mm:ss}</white>'
+                                              ' | <level>{level: <8}</level>'
+                                              ' | <white>{name}:{function}:{line}</white>'
+                                              ' | <white>{message}</white>')
