506b6a753daa884067965fe98632aedb48da3a00
alexindev <eruslanov-opt@yandex.ru>
2023-09-19 12:11:17 +0300

    create bot manager module

---
+++ bot.py
@@ -0,0 +1,16 @@
+from loader import dp, bot
+from handlers.commands import commands_handlers
+from handlers.admin import bot_stopped, bot_started
+
+
+async def start_bot():
+    """Запуск бота"""
+
+    commands_handlers(dp)
+    dp.startup.register(bot_started)
+    dp.shutdown.register(bot_stopped)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
