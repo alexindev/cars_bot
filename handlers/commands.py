1442b0df2dffc45b1d67323fd4610fa192b3fe48
alexindev <eruslanov-opt@yandex.ru>
2023-09-20 17:18:33 +0300

    update start_command handler

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,11 +1,19 @@
+from keyboard.standart import register_kb
+from keyboard.inline import main_kb
+from loader import bot, base
 from aiogram import types, Dispatcher
 from aiogram.filters import Command
-from loader import bot
 
 
 async def start_command(message: types.Message):
     """ Команда /start """
-    await bot.send_message(message.from_user.id, text='старт')
+    user = base.get_user(chat_id=message.from_user.id)
+    if user:
+        await bot.send_message(message.from_user.id, 'Главная страница', reply_markup=main_kb())
+    else:
+        await bot.send_message(message.from_user.id, 'Необходима регистрация.\n'
+                                                     'Для этого нажмите на кнопку ниже 👇',
+                               reply_markup=register_kb)
 
 
 async def help_command(message: types.Message):
