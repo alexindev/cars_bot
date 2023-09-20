4a1ad321e07f54069565a40c4a8aecde641e5234
alexindev <eruslanov-opt@yandex.ru>
2023-09-20 17:17:41 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
 from loader import dp, bot
 from handlers.commands import commands_handlers
-from handlers.admin import bot_stopped, bot_started
+from handlers.users import user_hanlers
+from keyboard.commands import set_commands
+from logs.config import logger
 
 
 async def start_bot():
     """Запуск бота"""
+    # dp.startup.register(bot_started)
+    # dp.shutdown.register(bot_stopped)
 
+    await set_commands()
+    user_hanlers(dp)
     commands_handlers(dp)
-    dp.startup.register(bot_started)
-    dp.shutdown.register(bot_stopped)
     try:
+        logger.info('Bot started')
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
-
