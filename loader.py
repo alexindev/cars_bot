07bf4c9993c722072eef4c1824ef8ab74bad636a
alexindev <eruslanov-opt@yandex.ru>
2023-09-20 13:17:01 +0300

    update loader

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from db.database import Database
 from dotenv import load_dotenv
 
 import os
@@ -9,3 +10,4 @@ load_dotenv()
 storage = MemoryStorage()
 bot = Bot(token=os.getenv('TOKEN'), parse_mode='HTML')
 dp = Dispatcher(storage=storage)
+base = Database()
