698a990b61a12900b7fc97534072397cee16660c
alexindev <eruslanov-opt@yandex.ru>
2023-09-22 10:46:07 +0300

    create new handlers

--- handlers/users.py
+++ handlers/users.py
@@ -1,8 +1,9 @@
 from aiogram import types, Dispatcher, F
 from aiogram.types import ReplyKeyboardRemove
 
-from keyboard.inline import settings_kb, seat_settings_kb, payment_settings_kb, delivery_settings_kb, main_kb
-from loader import bot, base
+from keyboard.inline import settings_kb, seat_settings_kb, payment_settings_kb, delivery_settings_kb, main_kb, \
+    statistic_kb, cancel_kb
+from loader import bot, base, data
 
 
 async def register_user(message: types.Message):
@@ -15,7 +16,41 @@ async def register_user(message: types.Message):
             reply_markup=ReplyKeyboardRemove()
         )
     else:
-        await bot.edit_message_text(message.from_user.id, message.message_id, 'Номер телефона не получен')
+        await bot.send_message(message.from_user.id, 'Номер телефона не получен')
+
+
+async def current_order(callback: types.CallbackQuery):
+    """ Текущий заказ """
+    await callback.message.edit_text('🤷‍♀ На данный момент нет активных заказов', reply_markup=cancel_kb())
+    await callback.answer()
+
+
+async def liderboard(callback: types.CallbackQuery):
+    """ Таблица лидеров """
+    liders: list = data.get_leaders()
+    if liders:
+        text = ''
+        for i, lider in enumerate(liders[:3], start=1):
+            fullname, orders = lider.split()[:2], lider.split()[-1]
+            medal = '🥇' if i == 1 else '🥈' if i == 2 else '🥉'
+            text += f'{medal} {" ".join(fullname)} - {orders} заказов\n'
+
+        if len(liders) >= 4:
+            for lider in liders[3:]:
+                fullname, orders = lider.split()[:2], lider.split()[-1]
+                text += f'🎗 {" ".join(fullname)} - {orders} заказов\n'
+
+        await callback.message.edit_text(text, reply_markup=cancel_kb())
+    else:
+        await callback.message.edit_text('Таблица лидеров на данный момент не сформирована',
+                                         reply_markup=cancel_kb())
+    await callback.answer()
+
+
+async def driver_statistic(callback: types.CallbackQuery):
+    """ Статистика водителя """
+    await callback.message.edit_text('Выберите пункт: 👇', reply_markup=statistic_kb())
+    await callback.answer()
 
 
 async def settings_user(callback: types.CallbackQuery):
@@ -51,6 +86,9 @@ async def cancel_menu(callback: types.CallbackQuery):
 def user_hanlers(dp: Dispatcher):
     """ Регистрация обработчиков """
     dp.message.register(register_user, F.contact)
+    dp.callback_query.register(current_order, F.data == 'current_order')
+    dp.callback_query.register(liderboard, F.data == 'leaderboard')
+    dp.callback_query.register(driver_statistic, F.data == 'user_stat')
     dp.callback_query.register(settings_user, F.data == 'settings')
     dp.callback_query.register(seat_settings, F.data == 'seat')
     dp.callback_query.register(payment_settings, F.data == 'payment')
