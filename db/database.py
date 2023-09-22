826c66c31f75cdafd883c6700806344333045089
alexindev <eruslanov-opt@yandex.ru>
2023-09-22 17:53:23 +0300

    update

--- db/database.py
+++ db/database.py
@@ -11,34 +11,35 @@ class Database:
         self.engine = create_engine(os.getenv('SQLALCHEMY_ENGINE'), echo=True)
         Base.metadata.create_all(bind=self.engine)
 
-    def register_user(self, chat_id: int, phone: str) -> bool:
+    def register_user(self, chat_id: int, phone: str, driver_id: str) -> bool:
         """
         Добавить пользователя
 
         :param chat_id: ChatID телеграм
         :param phone: Номер телефона
+        :param driver_id: Идентификатор водителя
         :return: True при успешном добавлении, False если пользователь не найден
         """
-        user = self.get_user(chat_id)
+        user = self.get_user(phone)
 
         if user:
             # Пользователь существует
             return False
         else:
             with Session(self.engine) as session:
-                session.add(User(chat_id=chat_id, phone=phone))
+                session.add(User(chat_id=chat_id, phone=phone, driver_id=driver_id))
                 session.commit()
                 return True
 
-    def get_user(self, chat_id: int) -> dict | None:
+    def get_user(self, phone: str) -> dict | None:
         """
         Получить пользователя
 
-        :param chat_id: ChatID телеграм
+        :param phone: Телефон
         :return: Словарь с данными пользователя или None
         """
         with Session(self.engine) as session:
-            stmt = select(User).where(User.chat_id == chat_id)
+            stmt = select(User).where(User.phone == phone)
             result = session.execute(stmt).fetchone()
             if result:
                 user = result[0]
--- loader.py
+++ loader.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from db.database import Database
-from utils.data import Data
+from utils.api_data import Data
 
 import os
 
