00212b037d07a3644466fd9f2da99a136a5e8cec
alexindev <eruslanov-opt@yandex.ru>
2023-09-24 21:52:44 +0300

    update

--- handlers/statistics.py
+++ handlers/statistics.py
@@ -9,30 +9,36 @@ from utils.helpers import get_statistic_text
 
 async def driver_statistic(callback: types.CallbackQuery):
     """ Меню статистики """
-    await callback.message.edit_text('Выберите пункт: 👇', reply_markup=statistic_kb())
     await callback.answer()
+    await callback.message.edit_text('Выберите пункт: 👇', reply_markup=statistic_kb())
 
 
 async def detail_statistic(callback: types.CallbackQuery):
     """ Детальная статистика за интервал """
+    await callback.answer()
+    await callback.message.edit_text('⏳ Собираем статистику...')
     user = base.get_user(chat_id=callback.from_user.id)
     if user:
         interval = '01'
         callback_data = callback.data.split('_')[-1]
+        date_to = datetime.today().strftime('%Y-%m-%d')
+        date_from = datetime.today().strftime('%Y-%m-01')
+
         if callback_data == 'day':
             interval = datetime.now().strftime('%d')
+            date_from = datetime.today().strftime('%Y-%m-%d')
+
         driver_id = user.get('driver_id')
+        cancelled = data.get_canceled_trip(driver_id=driver_id, date_from=date_from, date_to=date_to)
         stat = data.get_status(driver_id, interval)
+
         if stat:
-            text = get_statistic_text(stat)
+            text = get_statistic_text(stat, cancelled)
             await callback.message.edit_text(text=text, reply_markup=cancel_kb())
         else:
             logger.error('Данные по статистике на найдены')
-
     else:
         logger.error('Пользователь не найден')
-
-    await callback.answer()
 
 
 def statistics_handlers(dp: Dispatcher):
