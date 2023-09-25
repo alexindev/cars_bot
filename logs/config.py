8a280667efec352d053d6625771d1ec7b3ecf40f
alexindev <eruslanov-opt@yandex.ru>
2023-09-25 23:54:17 +0300

    update logger config

--- logs/config.py
+++ logs/config.py
@@ -1,7 +1,10 @@
 from loguru import logger
 from datetime import date
 
-logger.add(f'logs/{date.today()}.log', format='<white>{time:HH:mm:ss}</white>'
-                                              ' | <level>{level: <8}</level>'
-                                              ' | <white>{name}:{function}:{line}</white>'
-                                              ' | <white>{message}</white>')
+logger.add(f'logs/{date.today()}.log',
+           format='<white>{time:HH:mm:ss}</white>'
+                  ' | <level>{level: <8}</level>'
+                  ' | <white>{name}:{function}:{line}</white>'
+                  ' | <white>{message}</white>',
+           encoding='utf-8',
+           level='INFO')
