b66ef7108f6d3c4957d93deba182aa321e0cdebc
alexindev <eruslanov-opt@yandex.ru>
2023-09-26 15:55:08 +0300

    register new handler

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from loader import dp, bot
+from handlers.admin import admin_handlers
 from handlers.commands import commands_handlers
 from handlers.users import user_hanlers
 from handlers.settings import settings_handlers
@@ -8,13 +9,11 @@ from logs.config import logger
 
 async def start_bot():
     """Запуск бота"""
-    # dp.startup.register(bot_started)
-    # dp.shutdown.register(bot_stopped)
-
     user_hanlers(dp)
     commands_handlers(dp)
     settings_handlers(dp)
     statistics_handlers(dp)
+    admin_handlers(dp)
 
     try:
         logger.info('Bot started')
