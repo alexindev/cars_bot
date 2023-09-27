a9c48ff86eb9f94d0f33a39c282525140e842cbb
alexindev <eruslanov-opt@yandex.ru>
2023-09-27 14:47:25 +0300

    update name handler

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from loader import dp, bot
 from handlers.admin import admin_handlers
 from handlers.commands import commands_handlers
-from handlers.users import user_hanlers
+from handlers.users import user_handlers
 from handlers.settings import settings_handlers
 from handlers.statistics import statistics_handlers
 from logs.config import logger
@@ -9,7 +9,7 @@ from logs.config import logger
 
 async def start_bot():
     """Запуск бота"""
-    user_hanlers(dp)
+    user_handlers(dp)
     commands_handlers(dp)
     settings_handlers(dp)
     statistics_handlers(dp)
