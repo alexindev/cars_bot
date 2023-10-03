aab9e5220dbaab8e26504c2c9340f220ecafb4b8
alexindev <eruslanov-opt@yandex.ru>
2023-10-03 22:59:31 +0300

    new model a Park

--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
-from sqlalchemy import BigInteger
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+from sqlalchemy import BigInteger, ForeignKey
 
 
 class Base(DeclarativeBase):
@@ -14,8 +14,23 @@ class User(Base):
     car_id: Mapped[str]
     is_staff: Mapped[bool]
     full_name: Mapped[str]
+    park: Mapped['Park'] = relationship(back_populates='user')
+    park_id: Mapped[int] = mapped_column(ForeignKey('parks.id'))
 
     def __repr__(self):
         return (f'full_name: {self.full_name}, chat_id: {self.chat_id}, phone: {self.phone}, '
                 f'driver_id: {self.driver_id}, car_id: {self.car_id}, is_staff: {self.is_staff}')
 
+
+class Park(Base):
+    __tablename__ = 'parks'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    park_id: Mapped[str]
+    api_key: Mapped[str]
+    client: Mapped[str]
+    session_id: Mapped[str]
+    name: Mapped[str]
+    user: Mapped['User'] = relationship(back_populates='park', uselist=True)
+
+    def __repr__(self):
+        return f'park_id: {self.park_id} api_key: {self.api_key}, name: {self.name}'
