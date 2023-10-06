342144850899e2ba13c6562315ac6554e1145dc3
alexindev <eruslanov-opt@yandex.ru>
2023-10-06 22:44:26 +0300

    create new handler information

--- handlers/users.py
+++ handlers/users.py
@@ -5,7 +5,7 @@ from keyboard.inline import main_kb, cancel_kb
 from keyboard.standart import register_kb
 from loader import bot, data, base
 from utils.helpers import get_leaderboard_text, get_quality_text, current_order_data, unpaid_orders_text
-from utils.text_answer import main_menu
+from utils.text_answer import main_menu, information_tex
 
 
 async def authorization_user(message: types.Message):
@@ -33,9 +33,11 @@ async def authorization_user(message: types.Message):
         else:
             park_data = base.get_parks()
             for park in park_data:
-                session_id = park.get('session_id')
+                api_key = park.get('api_key')
                 park_id = park.get('park_id')
-                driver = data.get_driver_data(phone=phone, session_id=session_id, park_id=park_id)
+                client = park.get('client')
+                print(park.get('name'))
+                driver = await data.get_driver_list(phone=phone, api_key=api_key, park_id=park_id, client=client)
 
                 if driver:
                     base.register_user(chat_id=message.from_user.id, phone=phone, driver_id=driver[0], car_id=driver[1],
@@ -68,7 +70,7 @@ async def liderboard(callback: types.CallbackQuery):
 
     user = base.get_user(chat_id=callback.from_user.id)
     if user:
-        liders = data.get_leaders(park_id=user.get('park_id'), session_id=user.get('session_id'))
+        liders = await data.get_leaders(park_id=user.get('park_id'), session_id=user.get('session_id'))
         if liders:
             text = get_leaderboard_text(liders)
             await callback.message.edit_text(text, reply_markup=cancel_kb())
@@ -84,8 +86,8 @@ async def quality(callback: types.CallbackQuery):
     await callback.answer()
     user = base.get_user(chat_id=callback.from_user.id)
     if user:
-        quality_data = data.get_quality(driver_id=user.get('driver_id'), park_id=user.get('park_id'),
-                                        session_id=user.get('session_id'))
+        quality_data = await data.get_quality(driver_id=user.get('driver_id'), park_id=user.get('park_id'),
+                                              session_id=user.get('session_id'))
         if quality_data:
             text = get_quality_text(quality_data)
             await callback.message.edit_text(text=text, reply_markup=cancel_kb())
@@ -105,8 +107,8 @@ async def current_order(callback: types.CallbackQuery):
     user = base.get_user(chat_id=callback.from_user.id)
     if user:
         driver_id = user.get('driver_id')
-        order = data.get_current_order_status(driver_id=driver_id, park_id=user.get('park_id'),
-                                              session_id=user.get('session_id'))
+        order = await data.get_current_order_status(driver_id=driver_id, park_id=user.get('park_id'),
+                                                    session_id=user.get('session_id'))
         text = current_order_data(data=order, status=user.get('is_staff'))
         await callback.message.edit_text(text=text, reply_markup=cancel_kb())
     else:
@@ -119,12 +121,18 @@ async def unpaid_orders(callback: types.CallbackQuery):
     user = base.get_user(chat_id=callback.from_user.id)
     if user:
         await callback.message.edit_text('🔎 Собираем информацию...')
-        result = data.get_unpaid_orders(driver_id=user.get('driver_id'), park_id=user.get('park_id'),
-                                        session_id=user.get('session_id'))
+        result = await data.get_unpaid_orders(driver_id=user.get('driver_id'), park_id=user.get('park_id'),
+                                              session_id=user.get('session_id'))
         text = unpaid_orders_text(data=result)
         await callback.message.edit_text(text=text, reply_markup=cancel_kb())
     else:
         await callback.message.edit_text('❌ Сначала зарегистрируйтесь /start', reply_markup=cancel_kb())
+
+
+async def information(callback: types.CallbackQuery):
+    """ Ответы на частые вопросы """
+    await callback.answer()
+    await callback.message.edit_text(information_tex, reply_markup=cancel_kb())
 
 
 async def cancel_menu(callback: types.CallbackQuery):
@@ -140,4 +148,5 @@ def user_handlers(dp: Dispatcher):
     dp.callback_query.register(quality, F.data == 'quality')
     dp.callback_query.register(current_order, F.data == 'current_order')
     dp.callback_query.register(unpaid_orders, F.data == 'unpaid_orders')
+    dp.callback_query.register(information, F.data == 'information')
     dp.callback_query.register(cancel_menu, F.data == 'cancel')
