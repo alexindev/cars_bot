9685116e08ddc904b4c1d90dffc44ae1e791c465
alexindev <eruslanov-opt@yandex.ru>
2023-10-06 22:45:06 +0300

    create new reply keyboards

--- keyboard/standart.py
+++ keyboard/standart.py
@@ -22,7 +22,63 @@ send_phone_kb = ReplyKeyboardMarkup(
             KeyboardButton(text='📞 Отправить номер телефона', request_contact=True)
         ],
         [
-            KeyboardButton(text='↩ Отмена')
+            KeyboardButton(text='↩ Назад')
         ]
-    ], resize_keyboard=True, one_time_keyboard=True
+    ], resize_keyboard=True
 )
+
+information_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='ℹ Информация')
+        ]
+    ], resize_keyboard=True
+)
+
+select_career_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='🚕 Водитель такси')
+        ],
+        [
+            KeyboardButton(text='🚗 Автокурьер')
+        ],
+        [
+            KeyboardButton(text='🚶 Пеший курьер')
+        ],
+        [
+            KeyboardButton(text='↩ Назад')
+        ]
+    ], resize_keyboard=True
+)
+
+next_or_back = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Далее ▶')
+        ],
+        [
+            KeyboardButton(text='↩ Назад')
+        ]
+    ], resize_keyboard=True
+)
+
+cancel_reg_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='↩ Назад')
+        ]
+    ], resize_keyboard=True
+)
+
+
+def get_cities_kb(cities: list) -> ReplyKeyboardMarkup:
+    keyboard = []
+    for city in cities:
+        keyboard.append([KeyboardButton(text=f'{city.get("name")}')])
+    keyboard.append([KeyboardButton(text='↩ Назад')])
+    select_cities = ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
+    return select_cities
